27860b1a02318ee502c2b4bc13762d850c2a65df
sotikouk <sotikou@uth.gr>
2024-05-30 15:37:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/image2textapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.image2textapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button captureBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        captureBtn = findViewById(R.id.idBtnCapture);
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ScannerActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
